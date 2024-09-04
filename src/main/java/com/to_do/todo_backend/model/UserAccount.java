b1463a8ae040c82629718029939c83f2b24fe642
hasnacreate <pererahasini575@gmail.com>
2024-09-04 08:12:25 +0530

    changes to the user model

--- src/main/java/com/to_do/todo_backend/model/UserAccount.java
+++ src/main/java/com/to_do/todo_backend/model/UserAccount.java
@@ -25,5 +25,52 @@ public class UserAccount {
     @Column(name = "Age")
     private Integer age;
 
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
